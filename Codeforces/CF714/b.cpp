270bd892f4b2d349904aaaa0e62771bfc69750ac
Aaron Wang <wangaaron78739@gmail.com>
2025-02-16 22:32:39 -0500

    update

--- Codeforces/CF714/b.cpp
+++ Codeforces/CF714/b.cpp
@@ -17,22 +17,23 @@ using namespace std;
 
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) {
-	return os << '(' << p.first << ", " << p.second << ')';
+    return os << '(' << p.first << ", " << p.second << ')';
 }
-template <typename T_container,
-		  typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+template <typename T_container, typename T = typename enable_if<
+                                    !is_same<T_container, string>::value,
+                                    typename T_container::value_type>::type>
 ostream &operator<<(ostream &os, const T_container &v) {
-	os << '{';
-	string sep;
-	for (const T &x : v) os << sep << x, sep = ", ";
-	return os << '}';
+    os << '{';
+    string sep;
+    for (const T &x : v) os << sep << x, sep = ", ";
+    return os << '}';
 }
 
 void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void dbg_out(Head H, Tail... T) {
-	cerr << ' ' << H;
-	dbg_out(T...);
+    cerr << ' ' << H;
+    dbg_out(T...);
 }
 
 #ifdef AARON_DEBUG
@@ -44,48 +45,48 @@ void dbg_out(Head H, Tail... T) {
 const int64_t MOD = 1e9 + 7;
 
 int64_t fact(int n) {
-	int64_t x = 1;
-	for (int i = 1; i <= n; i++) {
-		x *= i;
-		x %= MOD;
-	}
-	return x;
+    int64_t x = 1;
+    for (int i = 1; i <= n; i++) {
+        x *= i;
+        x %= MOD;
+    }
+    return x;
 }
 
 void run_case() {
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	for (auto &x : a) cin >> x;
-	int t = a[0];
-	int zc = 0;
-	for (auto x : a) {
-		t &= x;
-	}
-	for (auto x : a) {
-		if (x == t) zc++;
-	}
-	if (zc < 2) {
-		cout << "0\n";
-		return;
-	}
-	int64_t m = fact(n - 2);
-	dbg(m);
-	m *= zc;
-	m %= MOD;
-	m *= (zc - 1);
-	m %= MOD;
-	cout << m << "\n";
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+    int t = a[0];
+    int zc = 0;
+    for (auto x : a) {
+        t &= x;
+    }
+    for (auto x : a) {
+        if (x == t) zc++;
+    }
+    if (zc < 2) {
+        cout << "0\n";
+        return;
+    }
+    int64_t m = fact(n - 2);
+    dbg(m);
+    m *= zc;
+    m %= MOD;
+    m *= (zc - 1);
+    m %= MOD;
+    cout << m << "\n";
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 #ifndef AARON_DEBUG
-	cin.tie(nullptr);
+    cin.tie(nullptr);
 #endif
 
-	int tests;
-	cin >> tests;
+    int tests;
+    cin >> tests;
 
-	while (tests-- > 0) run_case();
+    while (tests-- > 0) run_case();
 }
--- Codeforces/CF714/c.cpp
+++ Codeforces/CF714/c.cpp
@@ -17,22 +17,23 @@ using namespace std;
 
 template <typename A, typename B>
 ostream &operator<<(ostream &os, const pair<A, B> &p) {
-	return os << '(' << p.first << ", " << p.second << ')';
+    return os << '(' << p.first << ", " << p.second << ')';
 }
-template <typename T_container,
-		  typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+template <typename T_container, typename T = typename enable_if<
+                                    !is_same<T_container, string>::value,
+                                    typename T_container::value_type>::type>
 ostream &operator<<(ostream &os, const T_container &v) {
-	os << '{';
-	string sep;
-	for (const T &x : v) os << sep << x, sep = ", ";
-	return os << '}';
+    os << '{';
+    string sep;
+    for (const T &x : v) os << sep << x, sep = ", ";
+    return os << '}';
 }
 
 void dbg_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void dbg_out(Head H, Tail... T) {
-	cerr << ' ' << H;
-	dbg_out(T...);
+    cerr << ' ' << H;
+    dbg_out(T...);
 }
 
 #ifdef AARON_DEBUG
@@ -44,44 +45,44 @@ void dbg_out(Head H, Tail... T) {
 const int64_t MOD = 1e9 + 7;
 
 void run_case() {
-	string n;
-	int m;
-	cin >> n >> m;
-	array<int64_t, 10> freq = {};
-	for (char c : n) {
-		freq[c - '0']++;
-	}
-	int64_t ans = 0;
-	while (m >= 10) {
-		array<int64_t, 10> nfreq = {};
-		for (int i = 0; i < 9; i++) {
-			nfreq[i] += freq[i];
-			nfreq[(i + 1) % 10] += freq[i];
-		}
-		nfreq[0] += freq[9];
-		nfreq[1] += freq[9];
-		nfreq[9] += freq[9];
-		for (int i = 0; i < 10; i++) nfreq[i] %= MOD;
-		freq = nfreq;
-		m -= 10;
-	}
-	for (int i = 0; i < m; i++) {
-		int z = 10 - i - 1;
-		freq[z] *= 2;
-		freq[z] %= MOD;
-	}
-	for (int64_t z : freq) ans += z;
-	cout << ans << "\n";
+    string n;
+    int m;
+    cin >> n >> m;
+    array<int64_t, 10> freq = {};
+    for (char c : n) {
+        freq[c - '0']++;
+    }
+    int64_t ans = 0;
+    while (m >= 10) {
+        array<int64_t, 10> nfreq = {};
+        for (int i = 0; i < 9; i++) {
+            nfreq[i] += freq[i];
+            nfreq[(i + 1) % 10] += freq[i];
+        }
+        nfreq[0] += freq[9];
+        nfreq[1] += freq[9];
+        nfreq[9] += freq[9];
+        for (int i = 0; i < 10; i++) nfreq[i] %= MOD;
+        freq = nfreq;
+        m -= 10;
+    }
+    for (int i = 0; i < m; i++) {
+        int z = 10 - i - 1;
+        freq[z] *= 2;
+        freq[z] %= MOD;
+    }
+    for (int64_t z : freq) ans += z;
+    cout << ans << "\n";
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 #ifndef AARON_DEBUG
-	cin.tie(nullptr);
+    cin.tie(nullptr);
 #endif
 
-	int tests;
-	cin >> tests;
+    int tests;
+    cin >> tests;
 
-	while (tests-- > 0) run_case();
+    while (tests-- > 0) run_case();
 }
